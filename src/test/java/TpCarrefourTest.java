2b46efa84390c5a94594db600fc20fb5278611eb
hajar <hajar.ensao@gmail.com>
2022-09-12 11:55:21 +0200

    Update

---
+++ src/test/java/TpCarrefourTest.java
@@ -0,0 +1,39 @@
+import carrefour.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TpCarrefourTest {
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeMethod
+    public void stUp() {
+        driver.get("https://www.carrefour.fr/");
+        driver.manage().window().maximize();
+    }
+
+
+    @Test
+    public void test() {
+        //Arrange
+
+        //Act
+        HomePage homePage = new HomePage(driver);
+        homePage.acceptCookie().openMaisonsLoisirs().openRayons();
+
+
+        // Asserts
+
+
+    }
+
+
+    @AfterMethod
+    public void teardown() {
+//        driver.quit();
+
+
+    }
+}
